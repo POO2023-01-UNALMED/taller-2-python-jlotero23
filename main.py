2db5b33f835f5adaaaebff94e91bc39159494a98
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-02-19 22:36:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 class Asiento:
-
     def __init__(self, color, precio, registro):
         self.registro=registro
         self.color=color
         self.precio=precio
-
     def cambiarColor(self,color):
         colores = ["negro","blanco","rojo","verde","amarillo"]
         color.lower()
@@ -12,15 +10,12 @@ class Asiento:
             self.color=color
 
 class Motor:
-
     def _init_(self, numeroCilindros, tipo, registro):
         self.registro=registro
         self.tipo=tipo
         self.numeroCilindros=numeroCilindros
-
     def cambiarRegistro(self, registro):
         self.registro=registro
-
     def asignarTipo(self, tipo):
         tipos=["electrico","gasolina"]
         tipo.lower()
@@ -28,7 +23,6 @@ class Motor:
             self.tipo=tipo
 
 class Auto:
-
     def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
         self.modelo=modelo
         self.precio=precio
@@ -38,14 +32,12 @@ class Auto:
         self.registro=motor
         self.registro=registro
         self.cantidadCreados=cantidadCreados
-
     def cantidadAsientos(self):
         cant=0
         for i in self.asientos:
             if i!= None:
                 cant+=1
         return cant
-
     def verificarIntegridad(self):
         mensaje = "Las piezas no son originales"
         if self.registro==self.motor.registro:
