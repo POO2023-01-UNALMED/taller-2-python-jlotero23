8b10eeff057f930a19e06ec993a8acf1f3f2ea31
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-02-19 22:47:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 class Asiento:
     def __init__(self, color, precio, registro):
-        self.color=color
-        self.precio=precio
-        self.registro=registro
+        self.color = color
+        self.precio = precio
+        self.registro = registro
     def cambiarColor(self,color):
         colores = ["negro","blanco","rojo","verde","amarillo"]
         color.lower()
         if color in colores:
-            self.color=color
+            self.color = color
 
 class Auto:
-    cantidadCreados=0
+    cantidadCreados = 0
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
-        self.modelo=modelo
-        self.precio=precio
-        self.asientos=asientos
-        self.marca=marca
-        self.motor=motor
-        self.registro=motor
-        self.registro=registro
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
     def cantidadAsientos(self):
         cant=0
         for i in self.asientos:
@@ -37,15 +36,15 @@ class Auto:
 
 class Motor:
     def _init_(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros=numeroCilindros
-        self.tipo=tipo
-        self.registro=registro
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
     def cambiarRegistro(self, registro):
-        self.registro=registro
+        self.registro = registro
     def asignarTipo(self, tipo):
         tipos=["electrico","gasolina"]
         tipo.lower()
         if tipo in tipos:
-            self.tipo=tipo
+            self.tipo = tipo
 
 
